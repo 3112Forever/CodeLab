36a2151b1e14ba058dcc524f10340151104929e5
3112Forever <jknoproblem3112@gail.com>
2018-10-02 19:58:53 +0530

    Google Codelab UI Friend Chat - 4

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(new MaterialApp(
+    title: 'FriendChat',
+    debugShowCheckedModeBanner: false,
+    home: new Scaffold(
+      appBar: AppBar(
+        title: Text('Friend Chat'),
+      ),
+    ),
+  ));
+}
